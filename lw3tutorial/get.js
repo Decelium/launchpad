cd7813160d1afea2d7413f497518e4aac9df2bbe
Chris Hemming <christopher.hemming@alumni.utoronto.ca>
2022-09-26 18:21:24 +0000

    get.js and set.js

---
+++ lw3tutorial/get.js
@@ -0,0 +1,46 @@
+#!/usr/local/bin/node
+
+const ethers = require('ethers');
+
+const MoodContractAddress = "0x7c18621dad55Cf0C0254af0d8b966238808C22bc";
+const MoodContractABI =         [{"inputs":[],
+                                  "name":"getMood",
+                                  "outputs": [{"internalType":"string",
+                                              "name":"",
+                                              "type":"string"}],
+                                  "stateMutability":"view",
+                                  "type":"function"},
+                                 {"inputs":[{"internalType":"string",
+                                             "name":"_mood",
+                                             "type":"string"}],
+                                  "name":"setMood",
+                                  "outputs":[],
+                                  "stateMutability":"nonpayable",
+                                  "type":"function"}];
+let MoodContract;
+let signer;
+
+const provider = new ethers.providers.WebSocketProvider(
+    'wss://goerli.infura.io/ws/v3/555a93ec0c824ebe96a4d930dcf30124');
+
+const private_key = 'f2b6c1ba66e25eae8e6b3fcbab8f02e94cb942549bbede6cbd02f1b7d60ccad2';
+const wallet = new ethers.Wallet(private_key);
+const account = wallet.connect(provider);
+
+MoodContract = new ethers.Contract(
+    MoodContractAddress,
+    MoodContractABI,
+    account
+);
+
+async function getMood() {
+ const getMoodPromise = MoodContract.getMood();
+ const Mood = await getMoodPromise;
+ console.log(Mood);
+}
+
+function main() {
+    getMood();
+}
+
+main();
