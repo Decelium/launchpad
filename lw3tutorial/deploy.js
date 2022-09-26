c646dd11e2d0625d41f90a87558487359d14ab83
Chris Hemming <christopher.hemming@alumni.utoronto.ca>
2022-09-26 16:31:40 +0000

    First LearnWeb3.io tutorial.

---
+++ lw3tutorial/deploy.js
@@ -0,0 +1,25 @@
+#!/usr/local/bin/node
+
+const fs = require('fs');
+const ethers = require('ethers');
+
+
+bytecode = fs.readFileSync('mood_sol_MoodDiary.bin').toString();
+abi = JSON.parse(fs.readFileSync('mood_sol_MoodDiary.abi').toString());
+
+const provider = new ethers.providers.WebSocketProvider(
+    'wss://goerli.infura.io/ws/v3/555a93ec0c824ebe96a4d930dcf30124');
+const private_key = 'f2b6c1ba66e25eae8e6b3fcbab8f02e94cb942549bbede6cbd02f1b7d60ccad2';
+const wallet = new ethers.Wallet(private_key);
+const account = wallet.connect(provider);
+
+const myContract = new ethers.ContractFactory(abi, bytecode, account);
+
+async function main() {
+    const contract = await myContract.deploy();
+    
+    console.log(contract.address);
+    console.log(contract.deployTransaction);
+}
+
+main();
