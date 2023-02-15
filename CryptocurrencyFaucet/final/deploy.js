4023440470fc035859e808409a636b5248e39cd0
Chris Hemming <christopher.hemming@alumni.utoronto.ca>
2023-02-15 00:09:13 +0000

    debugging

--- CryptocurrencyFaucet/final/deploy.js
+++ CryptocurrencyFaucet/final/deploy.js
@@ -2,13 +2,13 @@
 
 const fs = require('fs');
 const ethers = require('ethers');
-
+require('dotenv').config({path: __dirname+"/../../.env"});
 
 bytecode = fs.readFileSync('faucetContract_sol_Token.bin').toString();
 abi = JSON.parse(fs.readFileSync('faucetContract_sol_Token.abi').toString());
 
-const provider = new ethers.providers.InfuraProvider("goerli",INFURA_API_KEY);
-const private_key = WALLET_PRIVATE_KEY;
+const provider = new ethers.providers.InfuraProvider("goerli",process.env.INFURA_API_KEY);
+const private_key = process.env.WALLET_PRIVATE_KEY;
 const wallet = new ethers.Wallet(private_key);
 const account = wallet.connect(provider);
 
